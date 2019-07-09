0441514ba6a2b8a5ef4411956eb3750acc14ce22
hussain <m.hussainul.islam@gmail.com>
2019-07-08 23:06:23 -0400

    board ready

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,17 +2,40 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        GridPane boardPane = new GridPane();
+        boardPane.setAlignment(Pos.BOTTOM_LEFT);
+        boardPane.setGridLinesVisible(true);
+        for (int row = 0; row < 6; row++){
+            for (int column = 0; column < 7; column++){
+                StackPane tokenPane = new StackPane();
+                Rectangle square = new Rectangle(50, 50, Color.GRAY);
+                Circle circleInset = new Circle(20);
+                circleInset.setFill(Color.WHITE);
+                tokenPane.getChildren().addAll(square, circleInset);
+                boardPane.add(tokenPane, column, row);
+            }
+        }
+
+
+
+
+        primaryStage.setTitle("Connect Four");
+        primaryStage.setScene(new Scene(boardPane, 500, 400));
         primaryStage.show();
     }
 
